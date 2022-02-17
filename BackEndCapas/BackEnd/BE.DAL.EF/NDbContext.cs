6084faadd3cedd17c018325c59b2b132876ad387
jaraya00356 <98362722+jaraya00356@users.noreply.github.com>
2022-02-16 21:06:50 -0600

    Semana 03
    semana 03 contiene errores

---
+++ BackEndCapas/BackEnd/BE.DAL.EF/NDbContext.cs
@@ -0,0 +1,46 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BE.DAL.EF
+{
+    public partial class NDbContext : DbContext
+    {
+        public NDbContext()
+        {
+
+        }
+
+        public NDbContext(DbContextOptions<NDbContext> options)
+           : base(options)
+        {
+        }
+
+        public virtual DbSet<Categories> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categories>(entity =>
+            {
+                entity.HasKey(e => e.CategoryId);
+
+                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+
+                entity.Property(e => e.CategoryName)
+                    .IsRequired()
+                    .HasMaxLength(15);
+
+                entity.Property(e => e.Description).HasColumnType("ntext");
+
+                entity.Property(e => e.Picture).HasColumnType("image");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    }
+}
